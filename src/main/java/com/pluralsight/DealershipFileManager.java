84dc9b87c1ba3926dd8fb159b1c64c6f5e3afa11
sourpunchlover <sndgpdesigns@gmail.com>
2026-05-08 10:59:00 -0400

    Created dealershipfilemanager

--- src/main/java/com/pluralsight/DealershipFileManager.java
+++ src/main/java/com/pluralsight/DealershipFileManager.java
@@ -1,4 +1,46 @@
 package com.pluralsight;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.regex.Pattern;
+
 public class DealershipFileManager {
+    public static Dealership getDealership() {
+        Dealership  dealership = new Dealership("Syds Super Cars", "111 Old Benbrook Rd", "817-555-5555");
+
+        try {
+            FileReader fileReader = new FileReader("src/main/resources/inventory.csv");
+            BufferedReader buffReader = new BufferedReader(fileReader);
+            buffReader.readLine();
+
+            String vehichleData;
+            buffReader.readLine();
+            while((vehichleData = buffReader.readLine()) != null) {
+                String[] splitVehicles = vehichleData.split(Pattern.quote("|"));
+
+                int vin = Integer.parseInt(splitVehicles[0]);
+                int year = Integer.parseInt(splitVehicles[1]);
+                String make = splitVehicles[2];
+                String model = splitVehicles[3];
+                String vehicleType = splitVehicles[4];
+                String color = splitVehicles[5];
+                int odometer = Integer.parseInt(splitVehicles[6]);
+                double price = Double.parseDouble(splitVehicles[7]);
+
+                Vehicle vehicle = new Vehicle(vin, year, odometer, make, model, vehicleType, color, price);
+                dealership.addVehicle(vehicle);
+            }//closing curly for while
+            buffReader.close();
+        }catch(IOException e) {
+            e.printStackTrace();
+            System.out.println("Problem with IO");
+        }//closing curly for catch
+        return dealership;
+    }
+
+    public static void saveDealership (Dealership dealership) {
+
+    }
 }
--- src/main/java/com/pluralsight/Vehicle.java
+++ src/main/java/com/pluralsight/Vehicle.java
@@ -1,4 +1,82 @@
 package com.pluralsight;
 
 public class Vehicle {
+    private int vin, year, odometer;
+    private String make, model, color, vehicleType;
+    private double price;
+
+    public Vehicle(int vin, int year, int odometer, String make, String model, String color, String vehicleType, double price) {
+        this.vin = vin;
+        this.year = year;
+        this.odometer = odometer;
+        this.make = make;
+        this.model = model;
+        this.color = color;
+        this.vehicleType = vehicleType;
+        this.price = price;
+    }
+
+    public int getVin() {
+        return vin;
+    }
+
+    public void setVin(int vin) {
+        this.vin = vin;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getOdometer() {
+        return odometer;
+    }
+
+    public void setOdometer(int odometer) {
+        this.odometer = odometer;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getVehicleType() {
+        return vehicleType;
+    }
+
+    public void setVehicleType(String vehicleType) {
+        this.vehicleType = vehicleType;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
