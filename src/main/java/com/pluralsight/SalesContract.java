ff631abb8287eb8542447fce5c05ecc5dd19634c
sourpunchlover <sndgpdesigns@gmail.com>
2026-05-15 15:49:19 -0400

    Updated override for salescontract

--- src/main/java/com/pluralsight/SalesContract.java
+++ src/main/java/com/pluralsight/SalesContract.java
@@ -24,12 +24,38 @@ public class SalesContract extends Contract {
     // override getTotalPrice()
     @Override
     public double getTotalPrice() {
-        return 0; // todo
+        double vehiclePrice = vehicleSold.getPrice();
+        double salesTax = vehiclePrice * 0.05;
+        double recordingFee = 100.00;
+        double processingFee;
+
+        if (vehiclePrice < 10000) {
+            processingFee = 295.00;
+        } else {
+            processingFee = 495.00;
+        }
+        return vehiclePrice + salesTax + recordingFee + processingFee;
     }
 
     // override getMonthlyPayment()
     @Override
     public double getMonthlyPayment() {
-        return 0; // todo
-    }
-}
+        if (!isFinanced) {
+             return 0;
+        }
+
+        double totalPrice = getTotalPrice();
+        double monthlyRate;
+        int months;
+
+        if (totalPrice >= 10000) {
+            monthlyRate = 0.0425 / 12;
+            months = 48;
+        } else {
+            monthlyRate = 0.0525 / 12;
+            months = 24;
+        }
+
+        double monthlyPayment = (totalPrice * monthlyRate) / (1 - Math.pow(1 + monthlyRate, -months));
+        return monthlyPayment;
+}   }
