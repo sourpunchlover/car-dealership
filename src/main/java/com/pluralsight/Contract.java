7869c0c83d04fb6b088e54a043fbfca06ccd2a97
sourpunchlover <sndgpdesigns@gmail.com>
2026-05-15 14:50:46 -0400

    Added Fields, constructor, getters & setters to Contract class

--- src/main/java/com/pluralsight/Contract.java
+++ src/main/java/com/pluralsight/Contract.java
@@ -1,4 +1,53 @@
 package com.pluralsight;
 
-public class Contract {
+public abstract class Contract {
+    protected String dateOfContract, customerName, customerEmail;
+    protected double totalPrice, monthlyPayment;
+    protected  boolean vehicleSold;//might have to change this & update getter & setter later
+
+    //constructor, all fields except total
+    //price and monthly payment
+    public Contract(String dateOfContract, String customerName, String customerEmail, boolean vehicleSold) {
+        this.dateOfContract = dateOfContract;
+        this.customerName = customerName;
+        this.customerEmail = customerEmail;
+        this.vehicleSold = vehicleSold;
+    }
+
+    //getters & setters
+
+    public String getDateOfContract() {
+        return dateOfContract;
+    }
+
+    public void setDateOfContract(String dateOfContract) {
+        this.dateOfContract = dateOfContract;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
+
+    public void setCustomerEmail(String customerEmail) {
+        this.customerEmail = customerEmail;
+    }
+
+    public boolean isVehicleSold() {
+        return vehicleSold;
+    }
+
+    public void setVehicleSold(boolean vehicleSold) {
+        this.vehicleSold = vehicleSold;
+    }
+
+    public abstract double getTotalPrice();
+    public abstract double getMonthlyPayment();
 }
